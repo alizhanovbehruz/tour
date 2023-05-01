549cfb22e01280eaa9ee3bce93282c48f0f3aef3
alizhanovbehruz <102398423+alizhanovbehruz@users.noreply.github.com>
2023-05-01 20:48:57 +0500

    'worktemp'

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# https://docs.django   project.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
